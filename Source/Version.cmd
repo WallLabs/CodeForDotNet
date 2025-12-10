f27a8ae54580ab50c68b5b61d1011f20bd3870de
Tony Wall <tony@tonywall.com>
2025-12-10 01:12:24 +0100

    Add PowerShell execution policy check to version script launch.

--- Source/Version.cmd
+++ Source/Version.cmd
@@ -1,1 +1,20 @@
-@powershell -File "%~dp0Version.ps1"
+@echo off
+setlocal
+
+rem Enable PowerShell execution for the current user.
+pwsh.exe -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned"
+if %errorlevel% neq 0 goto Error
+
+rem Execute PowerShell script.
+pwsh.exe -File "%~dp0Version.ps1"
+if %errorlevel% neq 0 goto Error
+
+rem Exit successfully.
+endlocal
+exit /b 0
+
+rem Error handler.
+:Error
+echo Error %errorlevel%!
+endlocal
+exit /b 1
