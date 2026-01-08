494971303795b0e498d759c529b4368290cf3746
Tony Wall <tony@tonywall.com>
2026-01-08 11:18:11 +0100

    Consolidate cross-platform functionality outside of Windows specific project.

---
+++ Network/NetworkExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CodeChief.Net
+{
+    /// <summary>
+    /// Contains helper methods and extensions for network operations.
+    /// </summary>
+    public static class NetworkExtensions
+    {
+        #region Constants
+
+        /// <summary>
+        /// First port on which the Wake-On-LAN broadcast is normally sent.
+        /// </summary>
+        public const int WakeOnLanPort1 = 0;
+
+        /// <summary>
+        /// Second port on which the Wake-On-LAN broadcast is normally sent.
+        /// </summary>
+        public const int WakeOnLanPort2 = 7;
+
+        /// <summary>
+        /// Third port on which the Wake-On-LAN broadcast is normally sent.
+        /// </summary>
+        public const int WakeOnLanPort3 = 9;
+
+        /// <summary>
+        /// Size of a Wake-On-LAN "magic packet" in bytes.
+        /// Contains a broadcast MAC (6 bytes, all 0xFF) followed by
+        /// the target MAC address (6 bytes / 48 bits) repeatd 16 times.
+        /// </summary>
+        public const int WakeOnLanPacketSize = MacAddress.Size + (MacAddress.Size * WakeOnLanPacketTargetMacCount);
+
+        /// <summary>
+        /// Number of times the target MAC address is repeated in the packet, excluding the prefix.
+        /// </summary>
+        public const int WakeOnLanPacketTargetMacCount = 16;
+
+        #endregion
+
+        #region Wake-On-LAN
+
+        /// <summary>
+        /// Broadcasts a Wake-On-LAN paket for the specified MAC address.
+        /// </summary>
+        public static void WakeOnLan(MacAddress mac)
+        {
+            // Create WOL packet
+            var packet = CreateWakeOnLanPacket(mac);
+
+            // Broadcast packet on all commonly used ports
+            var ports = new[] { WakeOnLanPort1, WakeOnLanPort2, WakeOnLanPort3 };
+            foreach (var port in ports)
+                BroadcastPacket(packet, port);
+        }
+
+        /// <summary>
+        /// Creates a Wake-On-LAN packet for the specified MAC address.
+        /// </summary>
+        public static byte[] CreateWakeOnLanPacket(MacAddress mac)
+        {
+            // Create packet buffer
+            var packet = new byte[WakeOnLanPacketSize];
+
+            // Add broadcast MAC address prefix
+            var broadcastMacBytes = new MacAddress(MacAddress.Broadcast).ToByteArray();
+            broadcastMacBytes.CopyTo(packet, 0);
+
+            // Add the target MAC address many times
+            var macBytes = mac.ToByteArray();
+            for (var i = 0; i < WakeOnLanPacketTargetMacCount; i++)
+                macBytes.CopyTo(packet, MacAddress.Size + (MacAddress.Size * i));
+
+            // Return result
+            return packet;
+        }
+
+        #endregion
+
+        #region UDP
+
+        /// <summary>
+        /// Sends a UDP (broadcast) packet to the specified mac address and port.
+        /// </summary>
+        public static void BroadcastPacket(byte[] packet, int port)
+        {
+            // Validate
+            if (packet == null) throw new ArgumentNullException(nameof(packet));
+            if (port <= 0) throw new ArgumentOutOfRangeException(nameof(port));
+
+            // Initialize a UDP client for broadcast on the specified port
+            using var client = new UdpClient();
+            client.Connect(IPAddress.Broadcast, port);
+
+            // Send packet
+            client.Send(packet, packet.Length);
+        }
+
+        #endregion
+    }
+}
---
+++ Threading/IWorkerHost.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace CodeChief.Threading
+{
+    /// <summary>
+    /// Worker host providing logging and cancellation.
+    /// </summary>
+    public interface IWorkerHost
+    {
+        /// <summary>
+        /// Trace source used for logging.
+        /// </summary>
+        TraceSource Log { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the worker should cancel its work.
+        /// </summary>
+        bool Canceled { get; }
+    }
+}
